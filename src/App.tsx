730258fa6bbb5cc0af6dcb6ff644eb91287b6e0c
felipeeco <contacto@felipemoreno.co>
2023-04-03 07:14:44 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { Routes, Route } from "react-router-dom"
+import { Container } from "react-bootstrap"
+import { Home } from "./pages/Home"
+import { Store } from "./pages/Store"
+import { About } from "./pages/About"
+import { Navbar } from "./components/Navbar"
+import { ShoppingCartProvider } from "./context/ShoppingCartContext"
+import { Login } from "./pages/Login"
+
+function App() {
+  return (
+    <ShoppingCartProvider>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Container>
+    </ShoppingCartProvider>
+  )
+}
+
+export default App
